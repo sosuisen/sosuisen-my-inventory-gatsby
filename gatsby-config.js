c8984361e12bef6237b370d58e1aea8362c34c0d
sosuisen <hidekazu.kubota@gmail.com>
2021-02-11 15:59:50 +0900

    [feature] Add first site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,29 @@
 module.exports = {
   siteMetadata: {
-    title: "My Gatsby Site",
+    title: "My Inventory Site",
   },
-  plugins: [],
+  plugins: [
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'item',
+        path: `${__dirname}/src/json/item/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'box',
+        path: `${__dirname}/src/json/box/`,
+      },     
+    },  
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'work',
+        path: `${__dirname}/src/json/work/`,
+      },     
+    },        
+  ],
 };
