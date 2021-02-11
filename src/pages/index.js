9649096a36527fd25bb6c61f58ffd2984df232ad
sosuisen <hidekazu.kubota@gmail.com>
2021-02-11 23:39:37 +0900

    [fix] Fix GraphQL scheme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,109 +1,14 @@
 import * as React from "react"
 import { graphql, StaticQuery, useStaticQuery } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: "300",
-  fontSize: "24px",
-  maxWidth: "560px",
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: "16px",
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: "14px",
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#000000",
-  },
-]
-
 // markup
 const IndexPage = () => {
-  const data = useStaticQuery(graphql`
+  const dbResult = useStaticQuery(graphql`
   query {
       allItemJson {
         edges {
           node {
+            _id
             name
             created_date
             modified_date
@@ -111,18 +16,48 @@ const IndexPage = () => {
           }
         }
       }
+      allBoxJson {
+        edges {
+          node {
+            _id
+            name
+            items
+          }
+        }
+      }
+      workJson {
+        _id
+        boxOrder
+        currentBox
+      }      
     }  
   `);
-  const edges = data.allItemJson.edges;
+  const itemHash = dbResult.allItemJson.edges.reduce((result, current) => {
+    result[current.node._id.replace('item/','')] = current.node;
+    return result;
+  }, {});
+  const boxHash = dbResult.allBoxJson.edges.reduce((result, current) => {
+    result[current.node._id.replace('box/','')] = current.node;
+    return result;
+  }, {});
+  const orderedBoxes = dbResult.workJson.boxOrder.map(boxId => boxHash[boxId]);
+
   return (
-    <main style={pageStyles}>
+    <main>
       <title>Home Page</title>
       <div>
-        {edges.map(e => (
-          <ul>
-            <li>{e.node.name}, {e.node.created_date}, {e.node.takeout ? '持出中' : '-'}</li>
-          </ul>
-        ))}
+        {orderedBoxes.map(box => (
+          <div>
+            <p>{box.name}</p>
+            {
+              box.items.map(itemId => (
+                <ul>
+                  <li>{itemHash[itemId].name}, {itemHash[itemId].created_date}, {itemHash[itemId].takeout ? '持出中' : '-'}</li>
+                </ul>
+              ))
+            }
+          </div>))
+        }
       </div>
     </main>
   )
