55fdce59121304c5796b937cba97dd63b68412ea
sosuisen <hidekazu.kubota@gmail.com>
2021-02-14 03:18:53 +0900

    fix: fix appearance of takeout

--- src/components/ItemRow.js
+++ src/components/ItemRow.js
@@ -18,7 +18,7 @@ const getLocalDateAndTime = (utcDateAndTime) => {
 const ItemRow = (props) => {
   return (<div className={props.index % 2 === 0 ? `${ItemRowStyle.row} ${ItemRowStyle.color_bg}` : ItemRowStyle.row}>
     <div className={`${ItemRowStyle.col} ${ItemRowStyle.takeout}`}>
-      {props.item.takeout ? '*' : '-'}
+      {props.item.takeout ? '持出中' : '-'}
     </div>
     <div className={`${ItemRowStyle.col} ${ItemRowStyle.name}`}><a name={props.item._id} href={`#${props.item._id}`} style={{textDecoration: 'none'}}>■</a>&nbsp;{props.item.name}</div>
     <div className={`${ItemRowStyle.col} ${ItemRowStyle.modified_date}`}>
