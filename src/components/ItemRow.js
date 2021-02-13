28826d93aea17b6d9b2ef0bfb5da4532c5176a35
sosuisen <hidekazu.kubota@gmail.com>
2021-02-14 04:48:48 +0900

    refactor: export getLocalDateAndTime function

--- src/components/ItemRow.js
+++ src/components/ItemRow.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ItemRowStyle from './ItemRow.module.css';
-
-const getLocalDateAndTime = (utcDateAndTime) => {
-  const regularUTC = utcDateAndTime.replace(
-    /^(\d\d\d\d-\d\d-\d\d).(\d\d:\d\d:\d\d)/,
-    '$1 $2'
-  );
-  const offset = new Date().getTimezoneOffset();
-  const utcMsec = Date.parse(regularUTC);
-
-  const localMsec = utcMsec - offset * 60 * 1000;
-  const localDate = new Date(localMsec);
-  const zeroPad = (num) => ('00' + num).slice(-2);
-  return `${localDate.getFullYear()}/${zeroPad(localDate.getMonth()+1)}/${zeroPad(localDate.getDate())} ${zeroPad(localDate.getHours())}:${zeroPad(localDate.getMinutes())}`;
-};
+import { getLocalDateAndTime } from '../utils/date';
 
 const ItemRow = (props) => {
   return (<div className={props.index % 2 === 0 ? `${ItemRowStyle.row} ${ItemRowStyle.color_bg}` : ItemRowStyle.row}>
---
+++ src/utils/date.js
@@ -0,0 +1,13 @@
+export const getLocalDateAndTime = (utcDateAndTime) => {
+  const regularUTC = utcDateAndTime.replace(
+    /^(\d\d\d\d-\d\d-\d\d).(\d\d:\d\d:\d\d)/,
+    '$1 $2'
+  );
+  const offset = new Date().getTimezoneOffset();
+  const utcMsec = Date.parse(regularUTC);
+
+  const localMsec = utcMsec - offset * 60 * 1000;
+  const localDate = new Date(localMsec);
+  const zeroPad = (num) => ('00' + num).slice(-2);
+  return `${localDate.getFullYear()}/${zeroPad(localDate.getMonth()+1)}/${zeroPad(localDate.getDate())} ${zeroPad(localDate.getHours())}:${zeroPad(localDate.getMinutes())}`;
+};
