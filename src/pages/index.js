4078559de2e77468eba7ded8b7c8ac724722fe11
sosuisen <hidekazu.kubota@gmail.com>
2021-02-14 04:49:07 +0900

    feat: add last modified time

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import ItemRow from '../components/ItemRow'
+import { getLocalDateAndTime } from '../utils/date';
 
 // markup
 const IndexPage = () => {
@@ -37,6 +38,14 @@ const IndexPage = () => {
     result[current.node._id.replace('item/','')] = current.node;
     return result;
   }, {});
+  const sortedItemEdges = [...dbResult.allItemJson.edges].sort((a, b) => {
+    if(a.node.modified_date > b.node.modified_date) return -1;
+    if(a.node.modified_date < b.node.modified_date) return 1
+    else return 0;
+  });
+  const lastModifiedDate = sortedItemEdges[0].node.modified_date;
+  console.log(lastModifiedDate);
+
   const boxHash = dbResult.allBoxJson.edges.reduce((result, current) => {
     result[current.node._id.replace('box/','')] = current.node;
     return result;
@@ -48,7 +57,7 @@ const IndexPage = () => {
       <title>疏水箱</title>
       <div id='top' style={{width: '1000px', marginLeft: 'auto', marginRight: 'auto'}}>
         <p>
-          疏水箱。
+          疏水箱。　　　<span style={{fontSize: '12px', color: '#909090'}}>最終更新：{getLocalDateAndTime(lastModifiedDate)}</span>
         </p>
         {orderedBoxes.map(box => (
           <span>[<a href={`#${box._id}`}>{box.name}</a>]&nbsp;&nbsp;</span>
