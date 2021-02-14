76691efce080320ee6a238685c684570d98ea2bc
sosuisen <hidekazu.kubota@gmail.com>
2021-02-14 18:33:55 +0900

    feat: display ... end of name when name length overflow

--- src/components/ItemRow.js
+++ src/components/ItemRow.js
@@ -7,7 +7,7 @@ const ItemRow = (props) => {
     <div className={`${ItemRowStyle.col} ${ItemRowStyle.takeout}`}>
       {props.item.takeout ? '持出中' : '-'}
     </div>
-    <div className={`${ItemRowStyle.col} ${ItemRowStyle.name}`}><a name={props.item._id} href={`#${props.item._id}`} style={{textDecoration: 'none'}}>■</a>&nbsp;{props.item.name}</div>
+    <div className={`${ItemRowStyle.col} ${ItemRowStyle.name}`} title={props.item.name}><a name={props.item._id} href={`#${props.item._id}`} style={{textDecoration: 'none'}}>■</a>&nbsp;{props.item.name}</div>
     <div className={`${ItemRowStyle.col} ${ItemRowStyle.modified_date}`}>
       {getLocalDateAndTime(props.item.modified_date).substr(0, 16)}
     </div>
